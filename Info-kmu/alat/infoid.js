522cdd1d2ac361ba83e2ef9521cce809ced446d4
w4hyou <wahyu243@gmail.com>
2017-10-23 01:47:20 +0700

    update.v.3

--- Info-kmu/alat/infoid.js
+++ Info-kmu/alat/infoid.js
@@ -1,6 +1,17 @@
 // Sebagian besar javascript di blog ini bersumber dari Blog DTE [http://www.dte.web.id] dan sebagian telah saya modifikasi. //<![CDATA[
+(function($) {
+    var $window = $(window),
+        $nav = $('nav'),
+        offset = $nav.offset().top;
+    $window.on("scroll", function() {
+        if (offset < $(this).scrollTop()) {
+            $nav.addClass('fixed');
+        } else {
+            $nav.removeClass('fixed');
+        }
+    });
+})(jQuery);
 (function(){$(document).ready(function(){return $(window).scroll(function(){return $(window).scrollTop()>600?$("#back-to-top").addClass("show"):$("#back-to-top").removeClass("show")}),$("#back-to-top").click(function(){return $("html,body").animate({scrollTop:"0"})})})}).call(this);
-
 
 var ww=document.body.clientWidth;$(document).ready(function(){$(".nav li a").each(function(){if($(this).next().length>0){$(this).addClass("parent")}});$(".toggleMenu").click(function(e){e.preventDefault();$(this).toggleClass("active");$(".nav").toggle()});adjustMenu()});$(window).bind("resize orientationchange",function(){ww=document.body.clientWidth;adjustMenu()});var adjustMenu=function(){if(ww<768){$(".toggleMenu").css("display","inline-block");if(!$(".toggleMenu").hasClass("active")){$(".nav").hide()}else{$(".nav").show()}$(".nav li").unbind("mouseenter mouseleave");$(".nav li a.parent").unbind("click").bind("click",function(e){e.preventDefault();$(this).parent("li").toggleClass("hover")})}else if(ww>=768){$(".toggleMenu").css("display","none");$(".nav").show();$(".nav li").removeClass("hover");$(".nav li a").unbind("click");$(".nav li").unbind("mouseenter mouseleave").bind("mouseenter mouseleave",function(){$(this).toggleClass("hover")})}};
 
